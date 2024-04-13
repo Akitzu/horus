a49ebaecfd1fe057b205f85d541f17a297e052cc
Akitzu <ludovic.rais@epfl.ch>
2024-04-13 15:15:28 +0200

    packaging with setuptools

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "horus",
+    version = "0.2.1",
+    author = "Ludovic Rais",
+    description = ("Helper package for pyoculus."),
+    packages=['horus'],
+)
